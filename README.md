ff6fc9926d9db2eae45cb62b3beae20c2f771a66
therkut <33721131+therkut@users.noreply.github.com>
2023-10-25 22:18:18 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Wiki Scraper**
+=========
+This is Twitter Bot written in python - using ```requests```, ```beautifulsoup```, ```OAuth1Session modules```. It loads the [Wikipedia page]
+(https://tr.wikipedia.org/wiki/Vikipedi:Tarihte_bug%C3%BCn) and then after scraping the *tweetable* content it tweets
+it to the Twitter account you specified in the script. Actually this script tweets events that occured today in the
+past. Setting it up is really easy, just enter your API Keys and Token Info in the ```main.py``` file and you are good to go.
+**It will be apprecaited if someone packages this**
---
+++ main.py
@@ -0,0 +1,82 @@
+import requests
+import os
+import time
+import datetime
+from requests_oauthlib import OAuth1Session
+from bs4 import BeautifulSoup
+from html import unescape
+
+# consumer_key = os.getenv('CONSUMER_KEY')
+# consumer_secret = os.getenv('CONSUMER_SECRET')
+# access_token = os.getenv('ACCESS_TOKEN')
+# access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+
+consumer_key = "YgPHyN4TEATBMcCx8PrwEAxr6a"
+consumer_secret = "1Hla1uMKbutfcU664XnLPXdOc1MopP78TNYKgtCGWfYBJOgwPJ"
+access_token = "2249982802-sLdoBqk2skVS2DDtTkCRgacgOynGZ2sA6jm62Zo"
+access_token_secret = "DFAJPZYpNh4Srsisb6R6ttwumfu1rZNISJYRN2i8SqEtD"
+
+
+bugun = datetime.date.today()
+aylar = ["", "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
+bu_ay = aylar[bugun.month]
+bu_gun = bugun.day
+hashtag = f"{{{bu_gun} {bu_ay}}} > #TarihteBugün"
+# print(hashtag)
+
+def strip_tags(html):
+    return unescape(BeautifulSoup(html, 'html.parser').text)
+
+def tweekibot():
+    try:
+        wiki = requests.get('https://tr.wikipedia.org/wiki/Vikipedi:Tarihte_bug%C3%BCn')
+        wiki_page = wiki.text
+        soup = BeautifulSoup(wiki_page, 'html.parser')
+
+        ul_elements = soup.find_all('ul')
+
+        if len(ul_elements) >= 2:
+            second_ul = ul_elements[-9]
+            list_items = second_ul.find_all('li')
+
+            # Liste öğelerini tersine çevir
+            list_items.reverse()
+            
+            tweet_list = []
+
+            for counter, line in enumerate(list_items):
+                if counter == 5:
+                    break
+                tweet = strip_tags(str(line))
+                tweet_list.append(tweet)
+
+            return tweet_list
+
+    except Exception as e:
+        print("Error while scraping Wikipedia:", e)
+        return []
+
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+tweet_list = tweekibot()
+tweet = "\n".join(tweet_list)  # Tweet listesini tek bir dizede birleştir
+
+for tweet in tweet_list:
+    payload = {"text": f"{tweet}\n\n{hashtag}"}
+
+    print("Payload: %s" % payload)
+
+    response = oauth.post("https://api.twitter.com/2/tweets", json=payload)
+    
+    if response.status_code != 200:
+        print("Request returned an error: {} {}".format(response.status_code, response.text))
+    else:
+        print("Tweet sent successfully")
+
+    time.sleep(20)  # 20 saniyelik aralık
