f5ae72dc00a169177d6a4f6c98f4a69d81e362fe
therkut <33721131+therkut@users.noreply.github.com>
2023-10-29 12:19:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ TWITTER_API_URL = 'https://api.twitter.com/2/tweets'
 WAIT_INTERVAL = 40  # 40-second interval
 TD_ELEMENT_ID = 'mp-itn'  # ID of the td element
 
-# Authenticate with Twitter API
+# Twitter API credentials
 consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-oauth = OAuth1Session(consumer_key, client_secret=consumer_secret, resource_owner_key=access_token, resource_owner_secret=access_token_secret)
 
 # Get today's date and create a hashtag with Turkish date format
 today = datetime.date.today()
@@ -27,14 +26,15 @@ this_month = months[today.month]
 this_day = today.day
 hashtag = f"{this_day} {this_month} > #Bugün #Tarih #Güncel #Bilgi #TarihteBugün"
 
-# Function to strip HTML tags from text
-def strip_tags(html):
-    return unescape(BeautifulSoup(html, 'html.parser').text)
 
-# Function to fetch Wikipedia data
+# Authenticate with Twitter API
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret, resource_owner_key=access_token, resource_owner_secret=access_token_secret)
+
+# Function to fetch Wikipedia data and create a reusable session
 def get_wikipedia_data():
+    session = requests.Session()  # Create a reusable session
     try:
-        response = requests.get(WIKIPEDIA_URL)
+        response = session.get(WIKIPEDIA_URL)
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
         wiki_page = response.text
         soup = BeautifulSoup(wiki_page, 'html.parser')
@@ -47,16 +47,22 @@ def get_wikipedia_data():
         tweet_list = [strip_tags(li.get_text()) for li in li_tags[:5]]
         return tweet_list
     except requests.exceptions.RequestException as e:
-        print("An error occurred while fetching data from Wikipedia: %s", str(e))
+        print("An error occurred while fetching data from Wikipedia:", str(e))
         return []
+    finally:
+        session.close()  # Close the session to release resources
+
+# Function to strip HTML tags from text
+def strip_tags(html):
+    return unescape(BeautifulSoup(html, 'html.parser').text)
 
 # Function to post a tweet
-def post_tweet(tweet_text, oauth):
+def post_tweet(tweet_text):
     try:
         payload = {"text": tweet_text}
         response = oauth.post(TWITTER_API_URL, json=payload)
         response.raise_for_status()
-        print("Tweet sent successfully: %s", tweet_text)
+        print("Tweet sent successfully:", tweet_text)
     except Exception as e:
         print("Request returned an error: {} {}".format(response.status_code, response.text))
 
@@ -66,7 +72,7 @@ def main():
     tweet_list = get_wikipedia_data()
     for tweet in tweet_list:
         tweet_text = f"{tweet}\n\n{hashtag}"
-        post_tweet(tweet_text, oauth)
+        post_tweet(tweet_text)
         time.sleep(WAIT_INTERVAL)
 
 if __name__ == '__main__':
